f88502a5d92b667230603dce53c08ffde247c024
ihuangxiaomin <1219549841@qq.com>
2025-02-19 17:06:40 +0800

    feat: 修改组件问题

--- src/components/ui/range-calendar/RangeCalendarHeading.vue
+++ src/components/ui/range-calendar/RangeCalendarHeading.vue
@@ -1,22 +1,28 @@
 <script lang="ts" setup>
-import { cn } from '@/lib/utils'
-import { RangeCalendarHeading, type RangeCalendarHeadingProps, useForwardProps } from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
+import { cn } from "@/lib/utils";
+import {
+  RangeCalendarHeading,
+  type RangeCalendarHeadingProps,
+  useForwardProps,
+} from "radix-vue";
+import { computed, type HTMLAttributes } from "vue";
 
-const props = defineProps<RangeCalendarHeadingProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<
+  RangeCalendarHeadingProps & { class?: HTMLAttributes["class"] }
+>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+  const { class: _, ...delegated } = props;
 
-  return delegated
-})
+  return delegated;
+});
 
-const forwardedProps = useForwardProps(delegatedProps)
+const forwardedProps = useForwardProps(delegatedProps);
 </script>
 
 <template>
   <RangeCalendarHeading
-    v-slot="{ headingValue }"
+    v-slot="{ headingValue }: any"
     :class="cn('text-sm font-medium', props.class)"
     v-bind="forwardedProps"
   >
